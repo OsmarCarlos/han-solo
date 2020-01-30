23408a006db08fcf967e1371a59b84d3a4be2196
Osmar Carlos <osmarcarlosrm@gmail.com>
2020-01-29 19:12:38 -0600

    Init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let app = {
+    title:  'Jojo Rabbit',
+    subtitle: 'Taika Waititi',
+    actors: ['Scarlett', 'Jhon']
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Actors = ({actors}) => (
+    <div>
+        {actors.map(actor => (
+            <li>{actor}</li>
+        ))}
+    </div>
+);
+
+let template = (
+    <div>
+        <h1>Movie: {app.title}</h1>
+        <p>Director: {app.subtitle}</p>
+        <ol>
+            <Actors actors={app.actors}></Actors>
+        </ol>
+    </div>
+);
+
+let user = {
+    name: 'Mike Pitt',
+    age: 22,
+    location: 'San Diego'
+}
+
+function getLocation (location) {
+    if(location) {
+        return location
+    } else {
+        return 'Unknow';
+    }
+};
+
+let templateTwo = (
+    <div>
+        <h1>{user.name.toUpperCase()}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {getLocation(user.location)}</p>
+    </div>
+);
+
+const container = document.getElementById('app');
+
+// ReactDom.render(__qué__, __dónde__);
+ReactDom.render(templateTwo, container);
