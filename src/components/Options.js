cda6db6295b80cb4e19433075da4893d2673e605
Osmar Carlos <osmarcarlosrm@gmail.com>
2020-02-28 15:01:02 -0600

    Finish styles for Indecision App

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,19 +3,28 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove all</button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
-        <ol>
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button 
+            onClick={props.handleDeleteOptions}
+            className="button button--link"
+            >
+                Remove all
+            </button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+        {/* <ol> */}
         {
             props.options.map((option, index) => (
             <Option 
-                optionText={option} 
+                optionText={option}
+                count={index + 1}
                 key={index}
                 handleDeleteOption={props.handleDeleteOption}
             />
             ))
         }
-        </ol>
+        {/* </ol> */}
     </div>
 )
 
